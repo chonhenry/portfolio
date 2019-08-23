88c39182d3897bc06c3d09dcc1789064fc5e0fee
Henry Chon <henry@macbook-pro.lan>
2019-08-23 18:38:20 -0400

    change titles to title in the model

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    titles = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/', blank=True)
