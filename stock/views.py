a0ba81b8ac25884effc697a4f818084f74d4f293
Henry Chon <henry@macbook-pro.lan>
2019-09-03 00:46:24 -0400

    stock app edited

--- stock/views.py
+++ stock/views.py
@@ -6,14 +6,18 @@ def home(request):
     import requests
     import json
 
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_59f2b887b82a45b298477654529b5f55")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_59f2b887b82a45b298477654529b5f55")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
 
-    return render(request, 'stock/home.html', {'api': api})
+        return render(request, 'stock/home.html', {'api': api})
+    else:
+        return render(request, 'stock/home.html', {'ticker': 'Enter a ticker symbol'})
 
 def about(request):
     return render(request, 'stock/about.html', {})
