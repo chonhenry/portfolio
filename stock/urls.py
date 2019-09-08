0fdeaf780607e290e1e5ee870f815b4ec37eff26
Henry Chon <chonhenry@y@utlook.com>
2019-09-07 23:55:04 -0400

    add stock

--- stock/urls.py
+++ stock/urls.py
@@ -4,4 +4,6 @@ from stock import views
 urlpatterns = [
     path('', views.home, name='stock'),
     path('about/', views.about, name='about'),
+    path('add_stock/', views.add_stock, name='add_stock'),
+    path('delete/<stock_id>', views.delete, name='delete'),
 ]
