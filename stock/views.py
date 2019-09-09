e9b187f17e551b6956b0bc48350e6ae4609ee04c
Henry Chon <chonhenry@y@utlook.com>
2019-09-09 01:04:40 -0400

    add stock functionality

--- stock/views.py
+++ stock/views.py
@@ -26,6 +26,9 @@ def about(request):
     return render(request, 'stock/about.html', {})
 
 def add_stock(request):
+    import requests
+    import json
+
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -35,13 +38,27 @@ def add_stock(request):
             return redirect('add_stock')
     else:
         ticker = Stock.objects.all()
-        return render(request, 'stock/add_stock.html', {'ticker': ticker})
+        output = []
+
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_59f2b887b82a45b298477654529b5f55")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+
+        return render(request, 'stock/add_stock.html', {'ticker': ticker, 'output': output})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, ("Stock has been deleted."))
     return redirect('add_stock')
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, 'stock/delete_stock.html', {'ticker':ticker})
 
 
 
